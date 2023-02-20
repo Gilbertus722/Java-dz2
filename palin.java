28b77d04aa3a3dc1daf932e30fb15a275e2d92b9
Gilbertus722 <112508781+Gilbertus722@users.noreply.github.com>
2023-02-20 18:49:49 +0300

    Add files via upload

---
+++ palin.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class palin {
+
+    public static void main(String[] args) {
+
+        Scanner f = new Scanner(System.in);
+
+        System.out.printf("Введите текст: ");
+        String word = f.nextLine();
+        String[] Chars = word.split("");
+        boolean result = CheckWord(Chars);
+
+        if (result == true) {
+            System.out.println("Это палиндром.");
+        } else {
+            System.out.println("Не верно");
+        }
+        f.close();
+    }
+
+    private static boolean CheckWord(String[] word) {
+
+        String[] Check = new String[word.length];
+        int count = 0;
+
+        for (int i = word.length - 1; i >= 0; i--) {
+
+            Check[count] = word[i];
+            count++;
+        }
+        String word1 = String.join("", word);
+        String Check1 = String.join("", Check);
+        boolean result = new String(Check1).equals(word1);
+
+        return result;
+    }
+}
